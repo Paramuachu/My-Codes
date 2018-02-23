a861e5daf0f2252417a4ef3b1fc5b3a67a881dff
Paramuachu <36722828+Paramuachu@users.noreply.github.com>
2018-02-23 11:35:55 +0530

    Create print "Hello".c

---
+++ print "Hello".c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int N,i;
+    printf("Enter the number :");
+    scanf("%d",&N);
+    for(i=1;i<=N;i++)
+    {
+        printf("Hello\n");
+    }
+    return 0;
+}
