b973951779bbfc271864e24f84b80240a2c7d5f7
Paramuachu <36722828+Paramuachu@users.noreply.github.com>
2018-02-23 10:10:58 +0530

    Create vOWEL OR CONSONANT.c

---
+++ vOWEL OR CONSONANT.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+  char c;
+  int a,b;
+  printf ("Enter a character :");
+  scanf ("%c", &c);
+  a =(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+  b =(c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
+  if(a||b)
+  {
+  printf("Vowel");
+  }
+  else
+  {
+  printf("Consonant");
+  }
+  return 0;
+}
