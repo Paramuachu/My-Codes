4ad851767499ac0f74de71d153b2d439aaf4840c
Paramuachu <36722828+Paramuachu@users.noreply.github.com>
2018-02-27 10:56:49 +0530

    Create even between two intervals.c

---
+++ even between two intervals.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,i;
+    printf("Enter the limits :");
+    scanf("%d%d",&a,&b);
+    for(i=a;i<=b;i++)
+    {
+    if(i%2==0)
+    printf("\t%d",i);
+    }
+    return 0;
+}
